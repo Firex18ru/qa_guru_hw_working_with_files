dcf7b959d66246665c8c6ea617ac268160e9e78e
Firex18ru <Firex1@yandex.ru>
2024-05-30 17:19:56 +0400

    Домашняя работа с архивами

--- confest.py
+++ confest.py
@@ -1,1 +1,21 @@
+import zipfile
+
 import pytest
+import shutil
+import os
+
+files_zip = ["tmp/Test_csv.csv", "tmp/Test_pdf.pdf", "tmp/Test_xls.xls"]
+
+
+@pytest.fixture(scope='function', autouse=True)
+def test_zip_folder():
+    files_zip = zipfile.ZipFile("tmp/folder.zip", "w")
+    files_zip.write("tmp/Test_csv.csv", arcname="Test_csv.csv", compress_type=zipfile.ZIP_STORED)
+    files_zip.write("tmp/Test_pdf.pdf", arcname="Test_pdf.pdf", compress_type=zipfile.ZIP_STORED)
+    files_zip.write("tmp/Test_xls.xls", arcname="Test_xls.xls", compress_type=zipfile.ZIP_STORED)
+    files_zip.close()
+
+    new_folder = "resources"
+    os.makedirs(new_folder, exist_ok=True)
+    yield
+    shutil.rmtree("resources")
