65133d5233735949c30b2d33279684a0bea54126
Firex18ru <Firex1@yandex.ru>
2024-05-31 19:14:48 +0400

    Домашняя работа с архивами

--- confest.py
+++ confest.py
@@ -1,22 +1,23 @@
-import zipfile
-
 import pytest
 import shutil
 import os
+from zipfile import ZipFile
 
 files_zip = ["tmp/Test_csv.csv", "tmp/Test_pdf.pdf", "tmp/Test_xls.xls"]
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture()
 def test_zip_folder():
-    files_zip = zipfile.ZipFile("tmp/folder.zip", "w")
-    files_zip.write("tmp/Test_csv.csv", arcname="Test_csv.csv", compress_type=zipfile.ZIP_STORED)
-    files_zip.write("tmp/Test_pdf.pdf", arcname="Test_pdf.pdf", compress_type=zipfile.ZIP_STORED)
-    files_zip.write("tmp/Test_xls.xls", arcname="Test_xls.xls", compress_type=zipfile.ZIP_STORED)
-    files_zip.close()
+    with ZipFile("folder.zip", "w") as myzip:
+        for file in files_zip:
+            myzip.write(file)
 
-    new_folder = "resources"
-    os.makedirs(new_folder, exist_ok=True)
+    # files_zip.write("tmp/Test_csv.csv", arcname="Test_csv.csv", compress_type=zipfile.ZIP_STORED)
+    # files_zip.write("tmp/Test_pdf.pdf", arcname="Test_pdf.pdf", compress_type=zipfile.ZIP_STORED)
+    # files_zip.write("tmp/Test_xls.xls", arcname="Test_xls.xls", compress_type=zipfile.ZIP_STORED)
+    # files_zip.close()
+
+    os.makedirs("resources", exist_ok=True)
     shutil.move("folder.zip", "resources/folder.zip")
     yield
     shutil.rmtree("resources")
--- test_with_files.py
+++ test_with_files.py
@@ -4,4 +4,10 @@ import csv
 import zipfile
 
 def test_pdf():
-    with zipfile.ZipFile("resources/folder.zip", "r") as zip_folder:
+    with ZipFile("resources/folder.zip", "r") as folder_zip:
+        with folder_zip.open("tmp/Test_pdf.pdf") as pdf_file:
+            reader = PdfReader(pdf_file)
+            page = len(reader.pages)
+        assert page == 3
+
+        folder_zip.close()
