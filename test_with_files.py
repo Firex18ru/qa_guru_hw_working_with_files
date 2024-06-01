c0c51a5ace1b2ec9b037bbd7a695d36abe80c4ae
Firex18ru <Firex1@yandex.ru>
2024-06-01 16:19:20 +0400

    Домашняя работа с архивами

--- test_with_files.py
+++ test_with_files.py
@@ -10,17 +10,19 @@ def test_pdf(test_zip_folder):
         with folder_zip.open("tmp/Test_pdf.pdf") as pdf_file:
             reader = PdfReader(pdf_file)
             page = len(reader.pages)
+            second_page_found = reader.pages[1]
+            text = second_page_found.extract_text()
+            expected_text = "Class aptent taciti sociosqu ad litora"
+            assert expected_text in text
             assert page == 3
-
-        folder_zip.close()
 
 
 def test_csv(test_zip_folder):
     with ZipFile("resources/folder.zip", "r") as folder_zip:
         with folder_zip.open("tmp/Test_csv.csv") as csv_file:
             reader = csv.reader(TextIOWrapper(csv_file, encoding="utf-8"))
-            return list(reader)
-            assert any("1, Dett, male,18,21/05/2015,Great Britain" in row for row in test_csv())
+            expected_row = ["1", "Dett", "Male", "18", "21/05/2015", "Great Britain"]
+            assert any(expected_row == row for row in reader)
 
 
 def test_xlsx(test_zip_folder):
@@ -31,6 +33,5 @@ def test_xlsx(test_zip_folder):
             data = []
             for row in sheet.iter_rows(values_only=True):
                 data.append(row)
-            return data
-            expected_row = "1, Dett, male,18,21/05/2015,Great Britain"
-            assert expected_row in test_xls
+            expected_row = (1, "Dett", "Male", 18, "21/05/2015", "Great Britain")
+            assert expected_row in data
